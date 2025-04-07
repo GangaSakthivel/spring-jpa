4c7a9aef3214a1f03973be3b544e36b4ee25cdbc
GangaSakthivel <gangasakthivel22@gmail.com>
2025-04-07 13:17:04 +0530

    Added Book Management Spring Boot project

---
+++ BookManagementSpring/BookManagementSpring/src/main/java/com/example/BookManagementSpring/BookManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookManagementSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementSpringApplication.class, args);
+	}
+
+}
