235fa9af37205c89d13bc2f5cd9e00d2a94d758e
GangaSakthivel <gangasakthivel22@gmail.com>
2025-04-07 13:36:34 +0530

    Books class were updated

---
+++ BookManagementSpring/BookManagementSpring/src/main/java/com/example/BookManagementSpring/entity/Book.java
@@ -0,0 +1,63 @@
+package com.example.BookManagementSpring.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String title;
+    private String author;
+    private long isbn;
+
+    public Book(){
+
+    }
+
+    public Book(long id, String title, String author, long isbn) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+}
+
